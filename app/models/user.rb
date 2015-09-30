203242674448d941eaee278754a5493e21733f3b
verycode <jamieoun@gmail.com>
2015-09-30 05:19:51 +0000

    carrierwave install

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_secure_password
+  mount_uploader :avatar, AvatarUploader
+
    def full_name
      "#{firstname} #{lastname}"
    end
+
 end
